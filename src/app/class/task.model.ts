9681223cb0209720fa279dcd521898c5916a7762
Cecilia-gr <seiika.8@gmail.com>
2022-11-28 12:37:46 +0100

    ajouter date

--- src/app/class/task.model.ts
+++ src/app/class/task.model.ts
@@ -3,11 +3,13 @@ id:number;
 title : string;
 complete : boolean;
 description : string;
+created : string;
 
   constructor(id : number=0, title:string="", complete:boolean=true, description:string="") {
     this.id = id;
     this.title = title;
     this.complete = complete;
     this.description = description;
+    this.created = Date();
   }
 }
